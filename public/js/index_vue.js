4d1a09da49d1b4a79035da5b6d31f965700debd5
sergmuzikant87 <sergmuzikant87@rambler.ru>
2022-06-05 18:58:25 +0300

    Сохранение 1

---
+++ public/js/index_vue.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        products: [],
+        products_filtered: [],
+        find_text:""
+    },
+    methods:{
+        get_products_from_server(){
+
+        },
+        filter_products(){
+
+        }
+    },
+    initial(){
+
+    }
+
+});
